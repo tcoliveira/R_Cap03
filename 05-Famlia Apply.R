9eeaf8770f1835a124b3d3d94c7d0e631c09829b
Thiago Carlos de Oliveira <thiagocarlos09@gmail.com>
2018-07-13 18:35:21 -0300

    Apply

---
+++ 05-Famlia Apply.R
@@ -0,0 +1,6 @@
+##Vetores = cadeia ordenadas de elementos
+#Loops são ineficientes no R, pode ser a familia apply
+
+#Apply, aplica uma função a todas as linhas / colunas de uma matriz / df
+#lapply, retorna nova lista / sapply
+#tapply 
