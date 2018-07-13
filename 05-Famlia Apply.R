701f3b6d2702d19b42c46d5af0fa9bc31f263628
Thiago Carlos de Oliveira <thiagocarlos09@gmail.com>
2018-07-13 18:39:39 -0300

    Apply

--- 05-Famlia Apply.R
+++ 05-Famlia Apply.R
@@ -4,3 +4,13 @@
 #Apply, aplica uma função a todas as linhas / colunas de uma matriz / df
 #lapply, retorna nova lista / sapply
 #tapply 
+
+
+
+#Usando loop
+lista1 <- list(a = (1:10), b = (45:77))
+?sapply
+sapply(lista1, mean)
+
+
+x <- matrix(rnorm(9), nr = 3, byrow = T)
